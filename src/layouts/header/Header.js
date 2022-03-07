37de61c533d7c1ec5eb4a661708a9fc969de076b
Peter <petermada@gmail.com>
2022-03-07 21:04:05 +0100

    Header layout

---
+++ src/layouts/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/images/peter-recipes-logo.jpg';
+import './styles.scss';
+
+export const Header = () => {
+  return (
+    <header className='header'>
+      <a href='/' className='logo'>
+        <img src={logo} alt='Peter Recipes homepage' />
+      </a>
+    </header>
+  );
+};
---
+++ src/layouts/main/Main.js
@@ -0,0 +1,10 @@
+import { Header } from '../header/Header';
+import './styles.scss';
+
+export const Main = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
